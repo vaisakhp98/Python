4ad8fde08be453dcf94e2aa06506c3c4f97b861c
vaisakhp98 <76683034+vaisakhp98@users.noreply.github.com>
2022-01-03 12:49:43 +0530

    countLetters

---
+++ countLetters.py
@@ -0,0 +1,12 @@
+stri = "VAISAKH"
+lists = list(stri)
+c = 0;
+
+for i in lists:
+    c = 0
+    if i!= " ":
+        for j in range (0,len(lists)):
+            if i == lists[j]:
+                c += 1
+                lists[j] = " "
+        print(f"{i} = {c}")
---
+++ multiplicationTable.py
@@ -0,0 +1,4 @@
+number = int(input("enter a number "))
+
+for i in range(1,10+1):
+    print(f"{number} x {i} = {number * i}")
