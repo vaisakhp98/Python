1b6f7627eae5fc0b9e39765a791de18a0725b3ee
vaisakhp98 <76683034+vaisakhp98@users.noreply.github.com>
2022-01-13 14:20:48 +0530

    function Odd or Even

---
+++ FuncOddorEven.py
@@ -0,0 +1,9 @@
+def oddOrEven(number):
+    
+    if number % 2 == 0:
+        print("number is Even")
+    else:
+        print("number is Odd")
+
+number = int(input("enter a number"))
+oddOrEven(number)
