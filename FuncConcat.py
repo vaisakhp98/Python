99fcbfb06d5b4af7c1a8ff37749c6e9ee6c4217d
vaisakhp98 <76683034+vaisakhp98@users.noreply.github.com>
2022-01-13 14:46:04 +0530

    FuncStringConcat

---
+++ FuncConcat.py
@@ -0,0 +1,7 @@
+def concatination(string1,string2):
+    print(f"the first string { string1} has been concatinated into { string2}")
+
+string1 = input("enter the 1st string ")
+string2 = input("enter the 2nd string ")
+
+concatination(string1,string2)
